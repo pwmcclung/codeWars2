aeadaa1964cad06cda4306676df05cf51b21a942
pwmcclung <pwmcclung@aol.com>
2021-01-05 11:05:21 -0800

    added dio

---
+++ dio.py
@@ -0,0 +1,12 @@
+import math
+def sol_equa(n):
+    response = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            j = n // i
+            if (i + j) % 2 == 0 and (j - i) % 4 == 0:
+                x = (i + j) // 2
+                y = (j - i) // 4
+                response.append([x, y])
+
+    return response
