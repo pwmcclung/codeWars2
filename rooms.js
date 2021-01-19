8fbf5ec0dcc770bfc44165f6f133ef8585ab10a9
pwmcclung <pwmcclung@aol.com>
2021-01-19 05:56:10 -0800

    added rooms

---
+++ rooms.js
@@ -0,0 +1,17 @@
+var rooms = {
+    room1: {
+       name: "roomOne",
+       description: "The first room ",
+       completed: "yes"
+     },
+     room2: {
+       name: "roomTwo",
+       description: "The second room",
+       completed: "no"
+     },
+     room3: {
+       name: "roomThree",
+       description: "the third room",
+       completed: "no"
+     }
+   }
