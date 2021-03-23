67449bef9af774b2372828a27f76952a55b3eaff
pwmcclung <pwmcclung@aol.com>
2021-03-23 06:56:17 -0700

    added loveVs to git

---
+++ loveVs.js
@@ -0,0 +1,9 @@
+function wordsToMarks(string){
+    let wordNum = { 'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 
+                  'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 
+                  'w':23,'x':24, 'y':25, 'z':26 }
+    let arr = string.split('')
+    let nums = arr.map(x => wordNum[x])
+    let numSum = nums.reduce((a, b) => a + b)
+    return numSum
+  }
