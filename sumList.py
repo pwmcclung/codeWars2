06cb9a2e1508af6f3e97bced5e5dcffb02f2cf3e
pwmcclung <pwmcclung@aol.com>
2021-04-05 06:50:36 -0700

    added sum list to git

--- sumList.py
+++ sumList.py
@@ -1,7 +1,12 @@
 
 def sum_no_duplicates(l):
+    #this line creates an empty list called newlist
     newlist = []
+    # this for loop iterates over the items in l
     for i in l:
+        # this if statement checks to see if the count of (i) is equal to 1, using the count function
         if l.count(i)== 1:
+            #if the count is 1 the item is appended to the newlist list
             newlist.append(i)
+    # this uses the sum function to calculate the sum of newlist and return the result
     return sum(newlist)
