e2dafd07ec05591614032abc44e479d3c973e31a
pwmcclung <pwmcclung@aol.com>
2021-04-06 08:14:34 -0700

    added commented summy code

--- summy.js
+++ summy.js
@@ -1,6 +1,8 @@
 function summy(stringOfInts){
-  
+    // this line uses the split method to split the string into an array called nums
     let nums = stringOfInts.split(" ")
+    // this line uses the reduce method to reduce the nums array and uses the parseFloat method to convert the string numvers into numbers, summing the array
     let newNum = nums.reduce((acc, cur) => acc + parseFloat(cur), 0)
+    // this returns the summed newNum variable
     return newNum
     }
