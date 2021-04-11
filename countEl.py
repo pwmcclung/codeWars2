22ef22828e0644f461672b93e6dd3fa5cc8c2c18
pwmcclung <pwmcclung@aol.com>
2021-04-11 06:52:03 -0700

    added count el

--- countEl.py
+++ countEl.py
@@ -1,8 +1,17 @@
+
+# this is in python 
 def count(array):
+    #this line initializes an empty dict
     elements = {}
+    # this is a for loop that iterates over the input array 
     for el in array:
+        # an if statement that asks if element is not in the elements dict
         if el not in elements:
+            # if it is not it adds the element as a key and adds one as its count
             elements[el] = 1
+        # and an else, if the element is in the elements dict
         else:
+            #this will add one to the already present element
             elements[el] += 1
+    # this line returns the elements dict
     return elements
