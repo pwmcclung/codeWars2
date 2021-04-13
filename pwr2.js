5c47529e60dfcf40a42cdad0cc492e1e96b2ace4
pwmcclung <pwmcclung@aol.com>
2021-04-13 05:55:58 -0700

    added power 2

--- pwr2.js
+++ pwr2.js
@@ -1,6 +1,8 @@
 function isPowerOfTwo(n){
+  // the if statement asks if n equals 0 and if so return false
     if (n == 0){
       return false
     }
+    // this statement returns n and n and -1 if n ==0
     return n && (n & (n - 1)) === 0
   }
