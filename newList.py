7137bd3d4fddbc62f12ce010b2b28693d62d0012
pwmcclung <pwmcclung@aol.com>
2021-04-12 17:17:49 -0700

    added new list to git

--- newList.py
+++ newList.py
@@ -1,7 +1,12 @@
 def no_odds(values):
+    # this line establishes an empty list
     newList = []
+    # this forloop iterates over the values array
     for x in values:
+        # if the element diviced by 2 is 0
         if x % 2 == 0:
+            # we append the element to the newlist list
             newList.append(x)
+    # return the new list
     return newList
             
