8c17c718a22c13b5aae33e920d822adbf72d24aa
pwmcclung <pwmcclung@aol.com>
2021-04-27 18:12:39 -0700

    added vapor1 to git

---
+++ vapor1.js
@@ -0,0 +1,15 @@
+function vaporcode(string) {
+    string = string.replace(/ /g, "")
+    string = string.toUpperCase()
+    let arr = string.split('')
+    let newArr = []
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] == arr.indexOf(arr[0])){
+        newArr.push(arr[i])
+      }else{
+        newArr.push('  ' )
+        newArr.push(arr[i])
+      }
+    }
+    return newArr.join('').trimStart()
+  }
