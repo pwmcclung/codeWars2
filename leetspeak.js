6255c2920ab3f97e5b14109ec515f9c5b9d39a4b
pwmcclung <pwmcclung@aol.com>
2021-05-04 09:14:04 -0700

    added leetspeak

--- leetspeak.js
+++ leetspeak.js
@@ -1,6 +1,11 @@
+// leet walkthrough
+// this line initializes a funciton 
 function toLeetSpeak(str) {
+    // this line uses the split method to turn the string into an array
+    //split method takes a pattern and divides a string into an ordered list of substrings by searching th 
+    //pattern and  putting them in to an array
     let arr = str.split('')
-    
+    // this line creates an object of the leet language
     let leet = {
    'A': '@',
     'B': '8',
@@ -30,7 +35,11 @@ function toLeetSpeak(str) {
     'Z' : '2',
       ' ': ' '
   }
+  // this line uses map method to iterate over the array and if the element is in leet, it exchanges the 
+  //element for the leet element
     arr = arr.map((e) => leet[e])
+    //this line uses the join method to turn the array into a string
     arr = arr.join('')
+    // this line returns the arr string 
     return arr;
   }
