683002d2e80cfd433e4ea93d152371ffbb0465a0
pwmcclung <pwmcclung@aol.com>
2021-05-05 08:28:30 -0700

    added name num

--- nameNum.js
+++ nameNum.js
@@ -1,11 +1,16 @@
+// code walkthrough 
+//this line creates the function with one parameter, num
 function nameThatNumber(num) {
+    //this line uses the String method to turn the number into a string
     let str = String(num)
+    //this line uses the split method on the string and turns it into an array
     let arr = str.split()
   //   let newArr = []
   //   for (let i = 0; i < arr.length; i++){
   //     newArr.push(Number(arr[i]))
   //   }
   //   console.log(newArr)
+  // this line creates an object of all numbers to 99/ 
     let numsDict = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 
                10:'ten',11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen',19: 'nineteen', 
                 20:'twenty', 21:'twenty one', 22:'twenty two', 23:'twenty three', 24:'twenty four', 25:'twenty five', 26:'twenty six', 27:'twenty seven', 28:'twenty eight', 29: 'twenty nine', 
@@ -16,7 +21,10 @@ function nameThatNumber(num) {
                 70:'seventy', 71:'seventy one', 72:'seventy two', 73:'seventy three', 74:'seventy four', 75:'seventy five', 76:'seventy six',77:'seventy seven', 78:'seventy eight', 79:'seventy nine' , 
                 80:'eighty', 81: 'eighty one', 82:'eighty two', 83:'eighty three', 84:'eighty four', 85:'eighty five', 86:'eighty six', 87:'eighty seven', 88:'eighty eight', 89:'eighty nine', 
                 90:'ninety', 91:'ninety one', 92:'ninety two', 93: 'ninety three', 94:'ninety four', 95:'ninety five', 96:'ninety six', 97:'ninety seven', 98:'ninety eight', 99:'ninety nine'}
-    return arr.map((e)=> numsDict[e] ).join('')
+    
+    // this line uses the map method on the arr array and maps the element in arr to the numsDict and then joins them into a string, then returns it
+                return arr.map((e)=> numsDict[e] ).join('')
   
     }
   
+    
