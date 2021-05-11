eb3dbd9d337c5843688ea6c55a67cc2169f21706
pwmcclung <pwmcclung@aol.com>
2021-05-11 05:47:18 -0700

    added sort array to js

--- sortArr1.js
+++ sortArr1.js
@@ -1,3 +1,7 @@
+//code walkthrough
+// this line initializes the function with one parameter, names
 sortme = function( names ){
+    //this line returns names after using the sort array method
+    //the sort() method sorts the elements in place and returns the sorted array
     return names.sort()
   }
