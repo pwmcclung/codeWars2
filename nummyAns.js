79d909ad1b8d1de7bea277f6b064e70e7e44987d
pwmcclung <pwmcclung@aol.com>
2021-05-27 05:49:14 -0700

    added nummy to git

--- nummyAns.js
+++ nummyAns.js
@@ -1,8 +1,15 @@
+// nummy walkthrough
+//this creates the function, with two params
 var findDigit = function(num, nth){
+    // an if statement to test if the nth is below zero and will return -1 if so
     if (nth <= 0){
+        //return -1
       return -1
     } 
+    // this creates the nummy variable by using the Math.abs function on num
     let nummy = "" + Math.abs(num);
+    // this creates the ans variable, uses slicing to get the correct value from nummy variable
     let ans =  nummy.slice(nummy.length - nth, nummy.length - nth + 1) * 1   
+    //return ans
     return ans
   }
