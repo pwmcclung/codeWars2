b8b4eb0e91fb2bb562556a9c348dd75b22f30f2f
pwmcclung <pwmcclung@aol.com>
2021-05-28 09:54:23 -0600

    added grow.js

--- grow.js
+++ grow.js
@@ -1,15 +1,24 @@
+//grow function walkthrough
+//this line creates the growingPlant function, three parameters
 function growingPlant(upSpeed, downSpeed, desiredHeight) {
+    //this line initializes the current height variable set to zero
     let currentHeight = 0;
+    //this line initializes the day variable
     let day;
+    //a for loop that counts the days
     for(day=1;;day++){
+        //current height is current height plus growth speed
     currentHeight += upSpeed;
+    //an if statement that evaluates if currentHeight is greater of equal to desiredHeight
     if(currentHeight >= desiredHeight){
+        //if so, break out of the loop
         break;
+        //if not, subtract downspeed for one day has passed
       } else { 
         currentHeight -= downSpeed;
       }
     }
-    
+    //return the number of days
     return day;
     
   }
