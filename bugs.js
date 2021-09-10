b54676d62abd3221e7bdd968581d600e019c8985
pwmcclung <pwmcclung@aol.com>
2021-09-10 06:12:31 -0600

    added bugs

---
+++ bugs.js
@@ -0,0 +1,13 @@
+function yourFutureCareer(){
+	let result = Math.random()
+		if (result <= 0.32) {
+			return 'FrontEnd Developer';
+		 }else if ( result <= 0.65){ 
+			return 'BackEnd Developer';
+		} else {
+			return 'Full-Stack Developer';
+		}
+  }
+
+
+yourFutureCareer();
