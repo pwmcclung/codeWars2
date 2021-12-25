778f1f8672723c5821a12dfdb3983c962577deb1
pwmcclung <pwmcclung@aol.com>
2021-12-24 20:37:06 -0700

    added first elements.js

---
+++ firstElements.js
@@ -0,0 +1,5 @@
+function first(arr, n=1) {
+    if (arr.length > 0){
+      return arr.slice(0, n)
+    }
+  }
---
+++ validateCode.js
@@ -0,0 +1,5 @@
+function validateCode (code) {
+    return String(code)[0] ==='1' || String(code)[0] === '2'|| String(code)[0]==='3'
+  
+  }
+  
