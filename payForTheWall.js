81a322fdf32e008fee8aeeddead32d39c7e47ca4
pwmcclung <pwmcclung@aol.com>
2021-12-31 19:12:51 -0700

    added pay for the wall

---
+++ payForTheWall.js
@@ -0,0 +1,15 @@
+function whoIsPaying(name){
+    if (name.length > 2){
+      let me = name.substring(0,2)
+   let newArr = []
+   newArr.push(name)
+     newArr.push(me)
+   return newArr
+      
+    }else if(name.length === 2){
+      return [name]
+    }else if (name.length ===1){
+      return [name]
+    }else {
+      return ['']
+    }
