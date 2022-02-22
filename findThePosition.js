4f9299a620747a7c56188ea7a7955b61d46afb26
pwmcclung <pwmcclung@aol.com>
2022-02-22 11:58:02 -0700

    added find the position to git

---
+++ findThePosition.js
@@ -0,0 +1,6 @@
+function position(letter){
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let pos = letters.indexOf(letter)
+    pos += 1
+    return `Position of alphabet: ${pos}`
+    }
