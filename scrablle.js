22d6094475f8400ff7fb28fc4d5796f6d033e588
pwmcclung <pwmcclung@aol.com>
2022-07-13 19:18:39 -0700

    added scabble

---
+++ scrablle.js
@@ -0,0 +1,12 @@
+function scrabbleScore(str){
+    str = str.toUpperCase().replace(/\s/g, "")
+    let score = {'A':1, 'B':3,'C':3, 'D':2, 'E':1, 'F': 4, 'G':2, 'H': 4, 'I':1, 'J':8, 'K':5, 'L': 1, 'M':3, 'N':1, 'O':1, 'P':3, 'Q': 10, 'R':1, 'S':1, 'T':1, 'U':1, 'V':4, 'W':4, 'X':8, 'Y':4, 'Z':10 }
+    let arr = str.split('')
+    let newArr = []
+    if (arr.length == 0){
+      return 0
+    }else{
+        newArr = arr.map((e)=> score[e])
+      }
+     return newArr.reduce((a, b) => a + b)
+    }
