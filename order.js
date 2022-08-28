804f74414c3f44b02fdae9a439935856c7578124
pwmcclung <pwmcclung@aol.com>
2022-08-28 05:49:34 -0700

    added order to git

--- order.js
+++ order.js
@@ -1,8 +1,12 @@
 function inAscOrder(arr) {
+    //this for loop starts the function, creates a variable for i
     for(let i = 0; i < arr.length - 1; i++) {
+        //this line is an if statement. it asks if arr[i] is greater than arr[i] plus 1
       if (arr[i] > arr[i+1]) {
+        //if so return false, this means the array is not in order
         return false; 
       }
     }
+    //else return true the array is in order
     return true;
   }
