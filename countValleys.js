c790045e5625d2c5584e4ad904d708e623a2c32a
pwmcclung <pwmcclung@aol.com>
2022-11-24 07:40:54 -0700

    added count valleys to git

---
+++ countValleys.js
@@ -0,0 +1,24 @@
+function countingValleys(s) {
+    let arr = s.split('')
+    let valleys = 0
+    let height = 0;
+    let inValley = false
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i ] == 'U'){
+        height += 1
+      }else if (arr[i] == 'D'){
+        height -= 1
+      }else{
+        continue
+      }
+       if (height < 0 ){
+      inValley = true
+    }
+    if (inValley == true && height >=0){
+      valleys += 1
+      inValley = false
+    }
+    }
+   
+    return valleys
+  }
